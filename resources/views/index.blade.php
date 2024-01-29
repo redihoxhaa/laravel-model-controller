976aaa7c4f39156fc82025506f6823669ff9f4d1
redihoxhaa <redi.hoxha1996@gmail.com>
2024-01-29 17:42:23 +0100

    feat: esercizio completato con bonus

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,26 @@
 @section('main')
     <ul class="row g-5">
         @foreach ($movies as $movie)
-            <li class="col-4">
-                <div class="card p-5 text-center">
-                    <h2 class="py-4 text-uppercase">{{ $movie['title'] }}</h2>
-                    <h5 class="pb-3">{{ $movie['original_title'] }}</h5>
-                    <p>An {{ $movie['nationality'] }} production</p>
-                    <p>Released on {{ \Carbon\Carbon::parse($movie['date'])->format('M d Y') }}</p>
-                    <p>With an overall score of {{ $movie['vote'] }} </p>
-
+            <li class="col-12 col-md-6 col-xl-4">
+                <div class="card-custom">
+                    <div class="tools">
+                        <div class="circle">
+                            <span class="red box"></span>
+                        </div>
+                        <div class="circle">
+                            <span class="yellow box"></span>
+                        </div>
+                        <div class="circle">
+                            <span class="green box"></span>
+                        </div>
+                    </div>
+                    <div class="card__content p-5">
+                        <h2 class="py-4 text-uppercase">{{ $movie['title'] }}</h2>
+                        <h5 class="pb-3">{{ $movie['original_title'] }}</h5>
+                        <p>An {{ $movie['nationality'] }} production</p>
+                        <p>Released on {{ \Carbon\Carbon::parse($movie['date'])->format('M d Y') }}</p>
+                        <p class="score">With an overall score of {{ $movie['vote'] }} </p>
+                    </div>
                 </div>
             </li>
         @endforeach
--- resources/views/subs/header.blade.php
+++ resources/views/subs/header.blade.php
@@ -1,3 +1,13 @@
 @section('header')
-    <h1 class="text-center py-5">Best movies of all time</h1>
+    <div class="terminal-loader">
+        <div class="terminal-header">
+            <div class="terminal-title">My new laravel DB</div>
+            <div class="terminal-controls">
+                <div class="control close"></div>
+                <div class="control minimize"></div>
+                <div class="control maximize"></div>
+            </div>
+        </div>
+        <div class="text">best movies of all time...</div>
+    </div>
 @endsection
